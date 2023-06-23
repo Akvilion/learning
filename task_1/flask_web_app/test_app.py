e879f62569ef7d42bb393eabd995b0d8e7efd12d
Akvilion <ianko.andrii@gmail.com>
2023-06-23 12:51:47 +0300

    action for deploy

--- task_1/flask_web_app/test_app.py
+++ task_1/flask_web_app/test_app.py
@@ -9,7 +9,7 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/')
-        self.assertEqual(rv.data, b'Hello, World!')
+        self.assertEqual(rv.data, b'Hello, World!111')
 
 if __name__ == '__main__':
     unittest.main()
