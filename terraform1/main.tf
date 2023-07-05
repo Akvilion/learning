bfad5dc9352d75d4f5e24734dd124201fe4c3e64
Akvilion <ianko.andrii@gmail.com>
2023-07-05 17:55:58 +0300

    5

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -1,12 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
   backend "s3" {
     bucket         = "e54t3h334g"
-    key            = "global/s3/terraform.tfstate"
+    key            = "state/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
 }
