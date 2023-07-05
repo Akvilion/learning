fcc08a18d7a26e32ea84bbbc241f3e79e9368d74
Akvilion <ianko.andrii@gmail.com>
2023-07-05 17:18:13 +0300

    Migrate from EC2 to ECS FARGATE

---
+++ terraform2/main.tf
@@ -0,0 +1,335 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "e54t3h334g"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# #################################################################
+# resource "aws_instance" "learning2" {
+#   ami                    = "ami-053b0d53c279acc90"
+#   instance_type          = "t2.micro"
+#   key_name               = "4820006"
+#   vpc_security_group_ids = [aws_security_group.learning2.id]
+
+#   tags = {
+#     Name = "two"
+#   }
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo apt update -y
+#     sudo apt install -y docker.io
+#     sudo service docker start
+#     sudo usermod -a -G docker ubuntu
+# EOF
+# }
+
+# resource "aws_security_group" "learning2" {
+#   name        = "WebServerSecurityGroup2"
+#   description = "My first SG2"
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     description = "SSH"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_instance" "learning1" {
+#   ami                    = "ami-053b0d53c279acc90"
+#   instance_type          = "t2.micro"
+#   key_name               = "4820006"
+#   vpc_security_group_ids = [aws_security_group.learning1.id]
+
+#   tags = {
+#     Name = "one"
+#   }
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo apt update -y
+#     sudo apt install -y docker.io
+#     sudo service docker start
+#     sudo usermod -a -G docker ubuntu
+# EOF
+# }
+
+# resource "aws_security_group" "learning1" {
+#   name        = "WebServerSecurityGroup1"
+#   description = "My first SG1"
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     description = "SSH"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+# #################################################################
+
+#################################################################ECR
+resource "aws_ecr_repository" "ecr_instance" {
+  name = "devops_course"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "learningpolicy" {
+  repository = aws_ecr_repository.ecr_instance.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 1 day",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+#################################################################
+
+
+
+#################################################################ECS
+
+# Гайд -> https://earthly.dev/blog/deploy-dockcontainers-to-awsecs-using-terraform/
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "app-cluster" # Name your cluster here
+}
+
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-first-task" # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "957271766300.dkr.ecr.us-east-1.amazonaws.com/devops_course:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# main.tf
+# Provide a reference to your default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Provide references to your default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  # Use your own region here but reference to subnet 1a
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  # Use your own region here but reference to subnet 1b
+  availability_zone = "us-east-1b"
+}
+
+# main.tf
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev" #load balancer name
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}"
+  ]
+  # security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Create a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id # default VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn #  load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn # target group
+  }
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "app-first-service"                  # Name the service
+  cluster         = aws_ecs_cluster.my_cluster.id        # Reference the created Cluster
+  task_definition = aws_ecs_task_definition.app_task.arn # Reference the task that the service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 2 # Set up the number of containers to 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn # Reference the target group
+    container_name   = aws_ecs_task_definition.app_task.family
+    container_port   = 5000 # Specify the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true                                                # Provide the containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "app_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
+
+########################## metrics.tf ############################
+
+resource "aws_cloudwatch_metric_alarm" "ECS_CPU_Usage_Alarm" {
+  alarm_name          = "ECS_CPU_Usage_Alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+
+  metric_name         = "CPUUtilization" # ActiveConnectionCount
+  namespace           = "AWS/ECS"
+  period              = "300"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "3" # якщо три 3 з 5 вище порогу то відправляємо алерт
+  statistic           = "Average"
+  threshold           = "70"
+  alarm_description   = "This metric monitors ecs cpu utilization exceeding 70%"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
+
+resource "aws_sns_topic" "alarms" {
+  name = "alarms"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.alarms.arn
+  protocol  = "email"
+  endpoint  = "codeorion12@gmail.com"
+}
