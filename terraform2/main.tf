6f8076e160357ec41b761dc9e8d948d1fc835a89
Akvilion <ianko.andrii@gmail.com>
2023-07-05 22:24:06 +0300

    16

---
+++ terraform2/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  backend "s3" {
+    bucket         = "e54t3h334g"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "learning" {
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
+  key_name               = "ubuntu"
+  vpc_security_group_ids = [aws_security_group.learning.id]
+
+  tags = {
+    Name = "learning"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install -y docker.io
+    sudo service docker start
+    sudo usermod -a -G docker ubuntu
+EOF
+}
+
+resource "aws_security_group" "learning" {
+  name        = "WebServerSecurityGroup"
+  description = "My first SG"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
