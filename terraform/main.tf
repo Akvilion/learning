c7b8fde5cf6859734be0db0cd05ee981c3dba963
Akvilion <ianko.andrii@gmail.com>
2023-07-06 12:27:25 +0300

    29

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,77 +2,30 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.5.0"
+      version = "~> 4.16"
     }
   }
   backend "s3" {
-    bucket = "433t3g3"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "e54t3h334g"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
   }
+  required_version = ">= 1.2.0"
 }
-
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "learning" {
-  ami                    = "ami-053b0d53c279acc90"
-  instance_type          = "t2.micro"
-  key_name               = "ubuntu"
-  vpc_security_group_ids = [aws_security_group.learning.id]
+#################################################################ECR
+resource "aws_ecr_repository" "ecr_instance" {
+  name                 = "devops_course"
 
-  tags = {
-    Name = "learning"
-  }
-  user_data = <<-EOF
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y docker.io
-sudo service docker start
-sudo usermod -a -G docker ubuntu
-EOF
-}
-
-resource "aws_security_group" "learning" {
-  name        = "WebServerSecurityGroup"
-  description = "My first SG"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
-
-# resource "aws_ecr_repository" "ecr_instance" {
-#   name                 = "learning"
-#   image_tag_mutability = "MUTABLE"
-
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-#   lifecycle {
-#     ignore_changes = all
-#   }
-# }
 
 resource "aws_ecr_lifecycle_policy" "learningpolicy" {
   repository = aws_ecr_repository.ecr_instance.name
@@ -82,7 +35,7 @@ resource "aws_ecr_lifecycle_policy" "learningpolicy" {
     "rules": [
         {
             "rulePriority": 1,
-            "description": "Expire images older than 1 days",
+            "description": "Expire images older than 1 day",
             "selection": {
                 "tagStatus": "untagged",
                 "countType": "sinceImagePushed",
@@ -97,9 +50,4 @@ resource "aws_ecr_lifecycle_policy" "learningpolicy" {
 }
 EOF
 }
-
-# output "ec2_ip" {
-#   value       = aws_instance.learning.public_ip
-#   description = "The public IP of the EC2 instance"
-# }
-
+#################################################################
--- terraform1/main.tf
+++ terraform1/main.tf
@@ -18,48 +18,82 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+# #################################################################
+# resource "aws_instance" "learning1" {
+#   ami                    = "ami-053b0d53c279acc90"
+#   instance_type          = "t2.micro"
+#   key_name               = "4820006"
+#   vpc_security_group_ids = [aws_security_group.learning1.id]
 
-resource "aws_instance" "learning1" {
-  ami                    = "ami-053b0d53c279acc90"
-  instance_type          = "t2.micro"
-  key_name               = "4820006"
-  vpc_security_group_ids = [aws_security_group.learning1.id]
+#   tags = {
+#     Name = "one"
+#   }
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo apt update -y
+#     sudo apt install -y docker.io
+#     sudo service docker start
+#     sudo usermod -a -G docker ubuntu
+# EOF
+# }
 
-  tags = {
-    Name = "one"
+# resource "aws_security_group" "learning1" {
+#   name        = "WebServerSecurityGroup1"
+#   description = "My first SG1"
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     description = "SSH"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+# #################################################################
+#################################################################ECR
+resource "aws_ecr_repository" "ecr_instance" {
+  name                 = "devops_course"
+
+  image_scanning_configuration {
+    scan_on_push = true
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y docker.io
-    sudo service docker start
-    sudo usermod -a -G docker ubuntu
+}
+
+resource "aws_ecr_lifecycle_policy" "learningpolicy" {
+  repository = aws_ecr_repository.ecr_instance.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 1 day",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
 EOF
 }
-
-resource "aws_security_group" "learning1" {
-  name        = "WebServerSecurityGroup1"
-  description = "My first SG1"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#################################################################
