f747cae6d01a8c9629d25ab2bffa10c3c7148c35
Akvilion <ianko.andrii@gmail.com>
2023-07-11 12:35:05 +0300

    Fargate examle 2

--- task_1/flask_web_app/test_app.py
+++ task_1/flask_web_app/test_app.py
@@ -9,7 +9,7 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/')
-        self.assertEqual(rv.data, b'Hello, me! Working! with ECS FARGATE! 11.07.2023 - 12:30')
+        self.assertEqual(rv.data, b'Hello, me! Working! with ECS FARGATE!')
 
 if __name__ == '__main__':
     unittest.main()
